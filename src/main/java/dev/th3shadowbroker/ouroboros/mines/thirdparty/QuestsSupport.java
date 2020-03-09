0883b297762be9739225051f4cc5b979f0b183fe
Th3Shadowbroker <webmaster@m4taiori.de>
2020-03-09 21:05:14 +0100

    Implemented Quests support

---
+++ src/main/java/dev/th3shadowbroker/ouroboros/mines/thirdparty/QuestsSupport.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 Jens Fischer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.th3shadowbroker.ouroboros.mines.thirdparty;
+
+import dev.th3shadowbroker.ouroboros.mines.OuroborosMines;
+import dev.th3shadowbroker.ouroboros.mines.events.MaterialMinedEvent;
+import me.blackvein.quests.Quester;
+import me.blackvein.quests.Quests;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+public class QuestsSupport implements Listener {
+
+    private static final String PLUGIN_NAME = "Quests";
+
+    public QuestsSupport() {
+        Bukkit.getPluginManager().registerEvents(this, OuroborosMines.INSTANCE);
+    }
+
+    private void addProgress(Player player, Material material) {
+        try
+        {
+            // Get plugin instance with the crowbar
+            Quests quests = (Quests) Bukkit.getPluginManager().getPlugin(PLUGIN_NAME);
+            Quester quester = quests.getQuester(player.getUniqueId());
+
+            // Check for matching quests
+            quester.getCurrentQuests().keySet().forEach(
+                    quest -> {
+                        // Modify progress manually
+                        quester.getCurrentStage(quest).getBlocksToBreak().stream().filter(itemStack -> itemStack.getType() == material).findFirst().ifPresent(
+                                itemStack -> {
+                                    quester.breakBlock(quest, new ItemStack(material));
+                                }
+                        );
+                    });
+        } catch (NullPointerException ex) {
+            OuroborosMines.INSTANCE.getLogger().warning("Unable to manipulate Quests quest-progress. It seems the plugin isn't installed anymore.");
+        }
+    }
+
+    public static boolean isAvailable() {
+        return Bukkit.getServer().getPluginManager().isPluginEnabled(PLUGIN_NAME);
+    }
+
+    @EventHandler
+    public void onMaterialMined(MaterialMinedEvent event) {
+        System.out.println("Material mined. Adding progress!");
+        addProgress(event.getPlayer(), event.getMaterial().getMaterial());
+    }
+
+}
