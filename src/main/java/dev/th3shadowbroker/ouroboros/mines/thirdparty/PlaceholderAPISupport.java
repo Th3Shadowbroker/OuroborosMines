1accb1b146e3a67a419e210e73942eb7107a8952
Th3Shadowbroker <webmaster@m4taiori.de>
2020-07-10 14:46:15 +0200

    Implemented placeholder ouroborosmines_oh

--- src/main/java/dev/th3shadowbroker/ouroboros/mines/thirdparty/PlaceholderAPISupport.java
+++ src/main/java/dev/th3shadowbroker/ouroboros/mines/thirdparty/PlaceholderAPISupport.java
@@ -20,8 +20,13 @@
 package dev.th3shadowbroker.ouroboros.mines.thirdparty;
 
 import dev.th3shadowbroker.ouroboros.mines.OuroborosMines;
+import dev.th3shadowbroker.ouroboros.mines.util.AnnouncementRunnable;
+import dev.th3shadowbroker.ouroboros.mines.util.RegionConfiguration;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.Optional;
 
 public class PlaceholderAPISupport extends PlaceholderExpansion {
 
@@ -44,15 +49,53 @@ public class PlaceholderAPISupport extends PlaceholderExpansion {
 
     @Override
     public String onPlaceholderRequest(Player player, String identifier){
-        String[] regionIdentifier = identifier.split("_");
-        String regionWorld = regionIdentifier[0];
-        String regionId = regionIdentifier[1];
+        String[] fragments = identifier.split("_");
+        if (fragments.length == 0) return "";
 
-        /*
-         * @TODO Add identifier interpretation here!
-         */
+        switch (fragments[0]) {
 
-        return "";
+            // For remaining time
+            case "oh":
+            case "opening":
+                String regionWorld = fragments[1];
+                String regionId = fragments[2];
+
+                // Check remaining time based on runnables that match in world and region.
+                long remainingTime = 0;
+                List<AnnouncementRunnable> matchingRunnables = OuroborosMines.INSTANCE.getAnnouncementManager().getRunnablesForRegion(regionWorld, regionId);
+                if (!matchingRunnables.isEmpty()) {
+
+                    // Get regional configuration
+                    Optional<RegionConfiguration> regionConfiguration = OuroborosMines.INSTANCE.getMaterialManager().getMineableMaterialOverrides().stream().filter(rc -> rc.getWorld().getName().equals(regionWorld) && rc.getRegionId().equals(regionId)).findFirst();
+                    if (regionConfiguration.isPresent()) {
+
+                        // Mines aren't already opened
+                        if (!regionConfiguration.get().minesAreOpen()) {
+                            // Find closest timer
+                            for (AnnouncementRunnable runnable : matchingRunnables) {
+                                if (remainingTime == 0 || runnable.getRemainingTime() < remainingTime) {
+                                    remainingTime = runnable.getRemainingTime();
+                                }
+                            }
+                            //                      ms -> s s->m m->h
+                            long h = (remainingTime / 1000  / 60 / 60) % 24;
+                            long m = (remainingTime / 1000 / 60) % 60;
+                            long s = (remainingTime / 1000) % 60;
+
+                            //@TODO Testing not finished. Remaining cases:
+                            // - Single opening hour
+
+                            return String.format("%02d:%02d:%02d", h , m, s);
+                        } else {
+                            return "Now!";
+                        }
+                    }
+                }
+
+            // Not specified
+            default:
+                return "";
+        }
     }
 
 }
