daf5ceac69e492c323d69fb633172d939426040d
Th3Shadowbroker <webmaster@m4taiori.de>
2020-07-23 11:02:12 +0200

    Implemented placeholder for naming mines (#28, #29)

--- src/main/java/dev/th3shadowbroker/ouroboros/mines/thirdparty/PlaceholderAPISupport.java
+++ src/main/java/dev/th3shadowbroker/ouroboros/mines/thirdparty/PlaceholderAPISupport.java
@@ -53,57 +53,68 @@ public class PlaceholderAPISupport extends PlaceholderExpansion {
         String[] fragments = identifier.split("_");
         if (fragments.length == 0) return "";
 
+        String regionWorld = fragments.length >= 2 ? fragments[1] : null;
+        String regionId = fragments.length >= 3 ? fragments[2] : null;
+
         switch (fragments[0]) {
 
             // For remaining time
             case "oh":
             case "opening":
                 // Abort if there are not enough fragments.
-                if (fragments.length < 3) return "Invalid placeholder!";
+                if (fragments.length >= 3) {
 
-                String regionWorld = fragments[1];
-                String regionId = fragments[2];
+                    // Check remaining time based on runnables that match in world and region.
+                    long remainingTime = 0;
+                    List<AnnouncementRunnable> matchingRunnables = OuroborosMines.INSTANCE.getAnnouncementManager().getRunnablesForRegion(regionWorld, regionId);
+                    if (!matchingRunnables.isEmpty()) {
 
-                // Check remaining time based on runnables that match in world and region.
-                long remainingTime = 0;
-                List<AnnouncementRunnable> matchingRunnables = OuroborosMines.INSTANCE.getAnnouncementManager().getRunnablesForRegion(regionWorld, regionId);
-                if (!matchingRunnables.isEmpty()) {
+                        // Get regional configuration
+                        Optional<RegionConfiguration> regionConfiguration = OuroborosMines.INSTANCE.getMaterialManager().getMineableMaterialOverrides().stream().filter(rc -> rc.getWorld().getName().equals(regionWorld) && rc.getRegionId().equals(regionId)).findFirst();
+                        if (regionConfiguration.isPresent()) {
 
-                    // Get regional configuration
-                    Optional<RegionConfiguration> regionConfiguration = OuroborosMines.INSTANCE.getMaterialManager().getMineableMaterialOverrides().stream().filter(rc -> rc.getWorld().getName().equals(regionWorld) && rc.getRegionId().equals(regionId)).findFirst();
-                    if (regionConfiguration.isPresent()) {
-
-                        // Mines aren't already opened
-                        if (!regionConfiguration.get().minesAreOpen()) {
-                            // Find closest timer
-                            for (AnnouncementRunnable runnable : matchingRunnables) {
-                                if (remainingTime == 0 || runnable.getRemainingTime() < remainingTime) {
-                                    remainingTime = runnable.getRemainingTime();
+                            // Mines aren't already opened
+                            if (!regionConfiguration.get().minesAreOpen()) {
+                                // Find closest timer
+                                for (AnnouncementRunnable runnable : matchingRunnables) {
+                                    if (remainingTime == 0 || runnable.getRemainingTime() < remainingTime) {
+                                        remainingTime = runnable.getRemainingTime();
+                                    }
                                 }
+
+                                long h = (remainingTime / 1000 / 60 / 60) % 24;
+                                long m = (remainingTime / 1000 / 60) % 60;
+                                long s = (remainingTime / 1000) % 60;
+
+                                String result = new TemplateMessage(OuroborosMines.INSTANCE.getConfig().getString("placeholders.openingHours.format", "%h%:%m%:%s%"))
+                                        .insert("h", String.format("%02d", h))
+                                        .insert("m", String.format("%02d", m))
+                                        .insert("s", String.format("%02d", s))
+                                        .colorize().toRawString();
+
+                                return result;
+                            } else {
+                                return TemplateMessage.from("placeholders.openingHours.open").colorize().toRawString();
                             }
-
-                            long h = (remainingTime / 1000  / 60 / 60) % 24;
-                            long m = (remainingTime / 1000 / 60) % 60;
-                            long s = (remainingTime / 1000) % 60;
-
-                            String result = new TemplateMessage(OuroborosMines.INSTANCE.getConfig().getString("placeholders.openingHours.format", "%h%:%m%:%s%"))
-                                            .insert("h", String.format("%02d", h))
-                                            .insert("m", String.format("%02d", m))
-                                            .insert("s", String.format("%02d", s))
-                                            .colorize().toRawString();
-
-                            //@TODO Testing not finished. Remaining cases:
-                            // - Single opening hour (Works!)
-                            //@FIXME The date doesn't get reset even though the runnables are scheduled for a 24h cycle, which leads to negative results.
-                            //@TODO Idea. Add one day when the current date lays in the past!
-                            //@TODO Hand over test build to tester
-
-                            //return String.format("%02d:%02d:%02d", h , m, s);
-                            return result;
-                        } else {
-                            return TemplateMessage.from("placeholders.openingHours.open").colorize().toRawString();
                         }
+                    } else {
+                        return "Missing opening hours for " + regionId;
                     }
+                } else {
+                    return "Invalid placeholder!";
+                }
+
+            case "name":
+            case "n":
+                // Abort if there are not enough fragments.
+                System.out.println(fragments.length);
+                if (fragments.length >= 3) {
+                    Optional<RegionConfiguration> regionConfiguration = OuroborosMines.INSTANCE.getMaterialManager().getMineableMaterialOverrides().stream().filter(rc -> rc.getWorld().getName().equals(regionWorld) && rc.getRegionId().equals(regionId)).findFirst();;
+                    if (regionConfiguration.isPresent()) {
+                        return regionConfiguration.get().getConfiguration().getString("name", regionConfiguration.get().getRegionId());
+                    }
+                } else {
+                    return "Invalid placeholder!";
                 }
 
             // Not specified
