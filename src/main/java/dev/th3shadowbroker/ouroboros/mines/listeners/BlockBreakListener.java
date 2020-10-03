2237e3ad54876c5b791f8a6583db94992cef0eca
Th3Shadowbroker <webmaster@m4taiori.de>
2020-10-03 10:08:03 +0200

    Implemented decreasing tool duration

--- src/main/java/dev/th3shadowbroker/ouroboros/mines/listeners/BlockBreakListener.java
+++ src/main/java/dev/th3shadowbroker/ouroboros/mines/listeners/BlockBreakListener.java
@@ -27,12 +27,15 @@ import dev.th3shadowbroker.ouroboros.mines.events.DepositDiscoveredEvent;
 import dev.th3shadowbroker.ouroboros.mines.events.MaterialMinedEvent;
 import dev.th3shadowbroker.ouroboros.mines.util.*;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Map;
 import java.util.Optional;
@@ -136,6 +139,25 @@ public class BlockBreakListener implements Listener {
                 overflow.forEach((slot, item) -> event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), item));
             }
         }
+
+        decreaseToolDurability(event.getPlayer(), tool);
+    }
+
+    private void decreaseToolDurability(Player player, ItemStack tool) {
+        Optional<ItemStack> miningTool = Optional.ofNullable(tool);
+        miningTool.ifPresent( itemStack -> {
+            ItemMeta meta = itemStack.getItemMeta();
+            if (meta instanceof Damageable) {
+                Damageable damageable = (Damageable) meta;
+                if (damageable.getDamage() + 1 < tool.getType().getMaxDurability()) {
+                    damageable.setDamage(damageable.getDamage() + 1);
+                    tool.setItemMeta((ItemMeta) damageable);
+                } else {
+                    player.getInventory().setItemInMainHand(null);
+                    player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1F, 1F);
+                }
+            }
+        });
     }
 
 }
