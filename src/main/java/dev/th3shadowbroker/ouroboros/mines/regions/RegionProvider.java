23ef489737830d0f7ed335f4c6d6f174626793aa
Th3Shadowbroker <webmaster@m4taiori.de>
2021-01-31 15:45:25 +0100

    Implemented RegionProvider, MiningRegion and AbstractFlag and a matching WorlGuard provider

---
+++ src/main/java/dev/th3shadowbroker/ouroboros/mines/regions/RegionProvider.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Jens Fischer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.th3shadowbroker.ouroboros.mines.regions;
+
+import dev.th3shadowbroker.ouroboros.mines.OuroborosMines;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import java.util.Optional;
+
+public abstract class RegionProvider {
+
+    protected final OuroborosMines plugin;
+
+    protected final String providerName;
+
+    protected final AbstractFlag flag;
+
+    public RegionProvider(String providerName, AbstractFlag flag) {
+        this.plugin = OuroborosMines.INSTANCE;
+        this.providerName = providerName;
+        this.flag = flag;
+    }
+
+    public abstract Optional<MiningRegion> getRegion(Location location);
+
+    public Optional<MiningRegion> getRegion(Block block) {
+        return getRegion(block.getLocation());
+    }
+
+    public abstract boolean isAvailable();
+
+    public abstract void onLoad();
+
+    public AbstractFlag getFlag() {
+        return flag;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public static Optional<RegionProvider> getProvider(RegionProvider... providers) {
+        for (RegionProvider provider : providers) {
+            if (provider.isAvailable()) return Optional.of(provider);
+        }
+        return Optional.empty();
+    }
+
+}
---
+++ src/main/java/dev/th3shadowbroker/ouroboros/mines/regions/providers/worldguard/WorldGuardProvider.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 Jens Fischer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.th3shadowbroker.ouroboros.mines.regions.providers.worldguard;
+
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import dev.th3shadowbroker.ouroboros.mines.regions.MiningRegion;
+import dev.th3shadowbroker.ouroboros.mines.regions.RegionProvider;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import java.util.Optional;
+
+public class WorldGuardProvider extends RegionProvider {
+
+    public WorldGuardProvider() {
+        super("WorldGuard", new WorldGuardFlag("ouroboros-mine"));
+    }
+
+    @Override
+    public Optional<MiningRegion> getRegion(Location location) {
+        Optional<World> world = Optional.ofNullable(location.getWorld());
+        if (world.isPresent()) {
+            Optional<RegionManager> regionManager = Optional.ofNullable(WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(world.get())));
+            if (regionManager.isPresent()) {
+                ApplicableRegionSet regionSet = regionManager.get().getApplicableRegions(BukkitAdapter.adapt(location).toVector().toBlockPoint());
+
+                ProtectedRegion region = null;
+
+                for (ProtectedRegion protectedRegion : regionSet.getRegions()) {
+                    if(region == null || protectedRegion.getPriority() > region.getPriority()) {
+                        region = protectedRegion;
+                    }
+                }
+
+                return Optional.ofNullable(region).map(r -> new MiningRegion(r.getId(), r.getFlag((StateFlag) flag.getPluginFlag()) == StateFlag.State.ALLOW));
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return plugin.getServer().getPluginManager().getPlugin(providerName) != null;
+    }
+
+    @Override
+    public void onLoad() {
+        WorldGuard.getInstance().getFlagRegistry().register((StateFlag) flag.getPluginFlag());
+    }
+
+}
